9e9f2829bc272cf176d2dbbe5d8217e19af72625
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2024-03-17 13:45:55 +0100

    Formatting

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,6 @@ use std::time::Instant;
 
 use shors_algorithm::_shor;
 
-
 fn main() {
     let user_input: u64 = 92349678913456;
 
@@ -13,5 +12,8 @@ fn main() {
     let sec = e.as_secs() as f64 + e.subsec_nanos() as f64 * 1e-9;
 
     println!("{:?}", set);
-    println!("The time taken to find the prime factors is {} seconds.", sec);
+    println!(
+        "The time taken to find the prime factors is {} seconds.",
+        sec
+    );
 }
