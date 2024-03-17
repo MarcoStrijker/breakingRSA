3a843c5c3a3c74f99ccc7a48f0bc133ef26795cc
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2024-03-17 11:30:31 +0100

    Create main.rs

---
+++ rust/src/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+use std::time::Instant;
+
+use shors_algorithm::_shor;
+
+
+fn main() {
+    let user_input: u64 = 92349678913456;
+
+    let s = Instant::now();
+    let set: HashSet<u64> = _shor(user_input);
+    let e = s.elapsed();
+    let sec = e.as_secs() as f64 + e.subsec_nanos() as f64 * 1e-9;
+
+    println!("{:?}", set);
+    println!("The time taken to find the prime factors is {} seconds.", sec);
+}
