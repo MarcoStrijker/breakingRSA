4df674d9eaa35de32324eab5024ca95348d30c73
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2024-07-14 18:50:23 +0200

    Update main.c

--- c_implementation/src/main.c
+++ c_implementation/src/main.c
@@ -191,6 +191,13 @@ i8 is_prime(u64 number) {
   // Returns:
   //     unsigned long long: 1 if the number is prime, 0 otherwise.
 
+  if (number % 2 == 0 || number == 1) {
+    // When the number is dividable by two, it is never
+    // a prime, except when it is two
+    return number == 2;
+  }
+
+
   i8 mem = get_from_dict(memorization_prime, number);
 
   // If mem is negative one, it means the number is not in the dictionary
@@ -198,7 +205,15 @@ i8 is_prime(u64 number) {
     return mem;
   }
 
-  for (u64 i = 2; i <= sqrt(number); i++) {
+  if (number % 3 == 0 || number % 5 == 0 || number % 7 == 0) {
+    return 0;
+  }
+
+  for (u64 i = 2; i <= sqrt(number); i+=2) {
+    if (i % 3 != 0 || i % 5 != 0 || i % 7 != 0) {
+      continue;
+    }
+
     if (number % i == 0) {
       add_to_dict(memorization_prime, number, 0);
       return 0;
@@ -209,189 +224,51 @@ i8 is_prime(u64 number) {
   return 1;
 }
 
-u64 gcd(u64 a, u64 b) {
-  // Calculate the greatest common divisor of two numbers.
-  //
-  // Args:
-  //     a (unsigned long long): The first number.
-  //     b (unsigned long long): The second number.
-  //
-  // Returns:
-  //     unsigned long long: The greatest common divisor of the two numbers.
-
-  while (b != 0) {
-    a %= b;
-    a ^= b;
-    b ^= a;
-    a ^= b;
-  }
-
-  return a;
-}
-
-u64 mod_exp(u64 base, u64 exponent, u64 modulus) {
-  // Calculate the modular exponentiation of a number.
-  //
-  // Args:
-  //     base (unsigned long long): The base.
-  //     exponent (unsigned long long): The exponent.
-  //     modulus (unsigned long long): The modulus.
-  //
-  // Returns:
-  //     unsigned long long: The modular exponentiation of the number.
-  if (modulus <= 1) {
-    return 0;
-  }
-
-  u64 result = 1;
-
-  while (exponent > 0) {
-    if (exponent % 2 == 1) {
-      result = (result * base) % modulus;
-    }
-
-    exponent = exponent >> 1;
-    base = (base * base) % modulus;
-  }
-
-  return result;
-}
-
-u64 make_guess(u64 number, u64 guess) {
-  // Make a guess to determine the factors of a number.
-  //
-  // Args:
-  //     number (int): The number for which the factors should be found.
-  //     guess (int): The guess for the factors.
-  //
-  // Returns:
-  //     A guess for the factors of the number.
-  while (gcd(number, guess) != 1) {
-    guess += 1;
-  }
-
-  return guess;
-}
-
-u64 calculate_exponent(u64 guess) {
-  // Calculate the exponent for the factors of a number.
-  //
-  // Args:
-  //     guess (int): The guess for the factors of the number.
-  //
-  // Returns:
-  //     The exponent for the factors of the number.
-  u64 r = 2;
-  u64 g = pow(guess, r);
-
-  while (g <= 1) {
-    r += 2;
-    g = pow(guess, r);
-  }
-
-  return r;
-}
-
-u64* find_factors(u64 number, u64 guess, u64 exponent) {
-  // Find the factors of a number. The output is always a prime.
-  //
-  // Args:
-  //     number (int): The number for which the factors should be found.
-  //     guess (int): The guess for the factors.
-  //     exponent (int): The exponent for the factors.
-  //
-
-  u64 temp;
-  // Free memory for array of the size of two u64
-  u64* factors = malloc(2 * sizeof(u64));
-
-  u64 nom = mod_exp(guess, exponent >> 1, number) + 1;
-  u64 den = number;
-  u64 outcome = gcd(nom, den);
-
-  if (outcome == number || outcome == 1) {
-    factors[0] = 0;
-    return factors;
-  }
-
-  while (!is_prime(number / outcome)) {
-    // Early exit if the denominator is 0
-    // This prevents a division by zero error
-    if (den == 0) {
-      factors[0] = 0;
-      return factors;
-    }
-    temp = den;
-    den = nom % den;
-    nom = temp;
-    outcome = gcd(nom, den);
-  }
-
-  factors[0] = number / outcome;
-  factors[1] = number / factors[0];
-
-  return factors;
-}
-
-Set* shor(u64 number) {
-  // Finding the prime factors of a number. For example, the prime factors of 15
-  // are 3 and 5. This is for breaking the RSA encryption algorithm.
-
-  // Args:
-  //     number (usigned long long): The number for which the prime factors
-  //     should be found.
-
-  // Returns:
-  //     The prime factors of the number.
-
-  // Only factors can be found for values higher than 2
-  // When the number itself is a prime, we just return the number and 1
+Set* find_prime_factors(u64 number) {
+  // We need to create the memorization_prime dictionary if it does not exist
   if (memorization_prime == NULL) {
     memorization_prime = create_dict();
     add_to_dict(memorization_prime, 0, 0);
     add_to_dict(memorization_prime, 1, 0);
+    add_to_dict(memorization_prime, 2, 1);
+    add_to_dict(memorization_prime, 3, 1);
+    add_to_dict(memorization_prime, 5, 1);
+    add_to_dict(memorization_prime, 7, 1);
   }
 
   if (number < 2 || is_prime(number)) {
     return initialize_set_with_single_item(number);
   }
-  // If the number is even, the prime factors are 2 and the prime factors of the
-  // other number
+
+  Set* factors = create_set(5);
+  u64 g = 3;
+
   if (number % 2 == 0) {
-    return add_u64_to_set(shor(number >> 1), 2);
+    factors = add_u64_to_set(factors, 2);
+
+    number >>= 1;
+    while (number % 2 == 0) {
+      number >>= 1;
+    }
   }
 
-  // Staring with a guess of 3
-  u64* factors;
-  u64 g = 3;
-  u64 r = calculate_exponent(g);
-
-  // Start the loop to find the prime factors
-  while (1) {
-    g = make_guess(number, g);
-    r = calculate_exponent(g);
-
-    factors = find_factors(number, g, r);
-
-    if (factors[0] <= 1) {
-      free(factors);
-      g += 1;
+  for (u64 g = 3; number != 1; g += 2) {
+    if (number % g == 0) {
+      factors = add_u64_to_set(factors, g);
+      number /= g;
       continue;
+    } 
+  
+    if (is_prime(g)) {
+      factors = add_u64_to_set(factors, number);
+      break;
     }
-
-    if (is_prime(factors[1])) {
-      return initialize_set_with_items(factors);
-    }
-
-    return add_u64_to_set(shor(factors[1]), factors[0]);
   }
 }
 
 
 int main() {
-  memorization_prime = create_dict();
-  add_to_dict(memorization_prime, 0, 0);
-  add_to_dict(memorization_prime, 1, 0);
+
 
   // Add way user can input number at runtime
   // The user can input a number for which the prime factors should be found.
