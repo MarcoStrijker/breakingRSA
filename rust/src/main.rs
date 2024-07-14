b111c9e889283f153a8e826d6428baf5fad7c19c
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2024-07-14 18:22:15 +0200

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,14 @@
 use std::collections::HashSet;
 use std::time::Instant;
 
-use shors_algorithm::find_prime_factors;
+use find_primes::_find_prime_factors;
 
 fn main() {
 
-    let user_input: u128 = 41282366920938463463307431768211415;
+    let user_input: u64 = 243;
 
     let s = Instant::now();
-    let set: HashSet<u128> = find_prime_factors(user_input);
+    let set: HashSet<u64> = _find_prime_factors(user_input);
     let e = s.elapsed();
     let sec = e.as_secs() as f64 + e.subsec_nanos() as f64 * 1e-9;
 
