c09d5303df12fa31885d428152586cadb0887c66
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2024-07-20 12:07:52 +0200

    Update main.pyi

--- mypyc_implementation/src/main.pyi
+++ mypyc_implementation/src/main.pyi
@@ -1,23 +1,13 @@
-""" Stub file for the Mypyc implementation of the Shor's algorithm.
-
-Functions:
-    is_prime: Check if a number is prime.
-    shor: Finding the prime factors of a number.
-
-"""
-
-def is_prime(number: int) -> int:
-    """Check if a number is prime. Stores result in a dictionary to speed up the process.
-
-    Args:
-        number (unsigned long long): The number to check if it is prime.
-
-    Returns:
-        A zero of a one, representing a boolean
-    """
+""" Stub file for the mypyc implementation of the Prime factor algorithm."""
 
 
-def shor(number: int) -> set[int]:
+from ctypes import c_ulonglong
+
+# Define aliases for the types used in the module
+u64 = c_ulonglong
+
+
+def find_prime_factors(number: u64) -> set[u64]:
     """ Finding the prime factors of a number. For example, the prime factors of 15 are 3 and 5.
     This is for breaking the RSA encryption algorithm.
 
